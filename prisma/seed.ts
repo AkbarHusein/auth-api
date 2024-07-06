ca62bec063d5b1e8b022fe77d44b5d5939e3b744
Van Husein <70703161+AkbarHusein@users.noreply.github.com>
2024-07-06 13:31:32 +0700

    perf: :art: "Added Prisma seed script to package.json"

---
+++ prisma/seed.ts
@@ -0,0 +1,26 @@
+import { hashSync } from 'bcryptjs'
+import pc from '../utils/prisma-client';
+
+async function main() {
+    console.log("<---------------- Seeding User ---------------->");
+
+    return await pc.user.upsert({
+        where: { email: 'admin@gmail.com' },
+        update: {},
+        create: {
+            email: 'admin@gmail.com',
+            name: 'admin',
+            password: hashSync('password', 10)
+        }
+    })
+
+}
+
+main().then(async (result) => {
+    console.log(result);
+    await pc.$disconnect()
+}).catch(async e => {
+    console.error(e)
+    await pc.$disconnect()
+    process.exit(1)
+})
